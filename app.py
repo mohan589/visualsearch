3cc3a0b22d6a6d9fdda179545d810e3e905b7db0
Mohan Pichikala <mpichikala@intralinks.com>
2024-10-20 19:13:46 +0530

    Fixed the image search as well.

--- app.py
+++ app.py
@@ -33,15 +33,20 @@ resizedImage = ''
 with st.sidebar:
   pic = st.file_uploader("Select File to Upload", type=['png', 'jpg', 'jpeg'])
   image = Image.open(pic) if pic else None
+  if image is not None:
+    canvas_width = 500
+    canvas_height = 400
+    image_width, image_height = image.size
+
   if pic:
     canvas_result = st_canvas(
       fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
       stroke_width=1,
       background_image=image,
       update_streamlit=True,
-      width=400,
-      height=550,
-      drawing_mode="circle",
+      width=canvas_width,
+      height=canvas_height,
+      drawing_mode="rect",
       key="canvas",
       display_toolbar=False
     )
@@ -49,17 +54,24 @@ with st.sidebar:
     if canvas_result.image_data is not None:
       objects = pd.json_normalize(canvas_result.json_data["objects"]) # need to convert obj to str because PyArrow
       if len(objects) > 0:
-        right = int(objects.tail(1)['left'].iloc[0]) + int(objects.tail(1)['width'].iloc[0]) + 100
-        bottom = int(objects.tail(1)['top'].iloc[0]) + int(objects.tail(1)['height'].iloc[0])
-        coordinateTuples = (int(objects.tail(1)['left'].iloc[0]), int(objects.tail(1)['top'].iloc[0]), right, bottom)
-        croppedImage = image.crop(coordinateTuples)
-        resizedImage = croppedImage
-        st.image(croppedImage, caption="Cropped Image", use_column_width=True, width=300)
+        # Scale the coordinates to match the original image size
+        scale_x = image_width / canvas_width
+        scale_y = image_height / canvas_height
+        
+        # Scale the coordinates
+        left_scaled = int(int(objects.tail(1)['left'].iloc[0]) * scale_x)
+        top_scaled = int(int(objects.tail(1)['top'].iloc[0]) * scale_y)
+        width_scaled = int(int(objects.tail(1)['width'].iloc[0]) * scale_x)
+        height_scaled = int(int(objects.tail(1)['height'].iloc[0]) * scale_y)
+        cropped_image = image.crop((left_scaled, top_scaled, left_scaled + width_scaled, top_scaled + height_scaled))
+        resizedImage = cropped_image
+        st.image(cropped_image, caption="Cropped Image", use_column_width=True, width=300)
       st.dataframe(objects)
 
 with st.sidebar:
   text_input = st.text_input("Search Text", "")
   if st.button("Search..."):
+    resizedImage = ''
     st.write(f"Searching for {text_input}!")
     text_embedding = clip.tokenize(text_input).to(device)
     text_features = model.encode_text(text_embedding).detach().cpu().numpy()
